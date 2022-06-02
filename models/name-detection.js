b4b4edea9cc9ffe45cefa0ab546b1160c2b3b620
Anes <anes.hekmatshoar@gmail.com>
2022-06-02 18:28:06 +0430

    adding new status 40

--- models/name-detection.js
+++ models/name-detection.js
@@ -1,50 +1,35 @@
 const axios = require("axios").default;
 
-async function getGender(name) {
-  try {
-    const response = await axios.get(`https://api.genderize.io/?name=${name}`);
-    return response.data.gender;
-  } catch (error) {
-    return error;
-  }
-}
-async function getNationality(name) {
-  try {
-    const response = await axios.get(
-      `https://api.nationalize.io/?name=${name}`
-    );
-    return response.data.country[0].country_id;
-  } catch (error) {
-    return error;
-  }
-}
-
-async function getDefinition(name) {
-  try {
-    const response = await axios.get(
-      `https://api.urbandictionary.com/v0/define?term=${name}`
-    );
-    let definitionsArray = response.data.list.map((item) => {
-      return item.definition;
-    });
-    return definitionsArray;
-  } catch (error) {
-    console.log(error);
-    return error;
-  }
-}
-
 module.exports = {
-  nameDetection: async (name) => {
-    let gender = await getGender(name);
-    let country = await getNationality(name);
-    let definitions = await getDefinition(name);
-    let nameInformation = {
-      name: name,
-      gender: gender,
-      country_id: country,
-      definitions: definitions,
-    };
-    return nameInformation;
+  getGender: async (name) => {
+    try {
+      const response = await axios.get(
+        `https://api.genderize.io/?name=${name}`
+      );
+      return response.data;
+    } catch (error) {
+      return error;
+    }
+  },
+  getNationality: async (name) => {
+    try {
+      const response = await axios.get(
+        `https://api.nationalize.io/?name=${name}`
+      );
+      return response.data;
+    } catch (error) {
+      return error;
+    }
+  },
+  getDefinition: async (name) => {
+    try {
+      const response = await axios.get(
+        `https://api.urbandictionary.com/v0/define?term=${name}`
+      );
+
+      return response.data;
+    } catch (error) {
+      return error;
+    }
   },
 };
--- routes/names.js
+++ routes/names.js
@@ -5,9 +5,31 @@ var nameDetection = require("../models/name-detection");
 
 /* GET users listing. */
 router.get("/:name", async function (req, res, next) {
-  result = await nameDetection.nameDetection(req.params.name);
-  res.status(200);
-  res.json(result);
+  genderResponse = await nameDetection.getGender(req.params.name);
+  nationalityResponse = await nameDetection.getNationality(req.params.name);
+  definitionResponse = await nameDetection.getDefinition(req.params.name);
+
+  let definitionsArray = definitionResponse.list.map((item) => {
+    return item.definition;
+  });
+  if (
+    genderResponse.gender === null ||
+    nationalityResponse.country.length === 0 ||
+    definitionsArray.length === 0
+  ) {
+    res.status(404);
+    res.send('Not Found');
+  } else {
+    console.log("here");
+    let nameInformation = {
+      name: req.params.name,
+      gender: genderResponse.gender,
+      country_id: nationalityResponse.country[0].country_id,
+      definitions: definitionsArray,
+    };
+    res.status(200);
+    res.json(nameInformation);
+  }
 });
 
 module.exports = router;
